a9d3c664bdca4c56e40df0601845855ad4d29464
Pim Borman <pimborman2@gmail.com>
2017-06-08 14:12:33 +0200

    save stuff

--- Module6/assignment6.py
+++ Module6/assignment6.py
@@ -4,94 +4,72 @@ import time
 # Grab the DLA HAR dataset from:
 # http://groupware.les.inf.puc-rio.br/har
 # http://groupware.les.inf.puc-rio.br/static/har/dataset-har-PUC-Rio-ugulino.zip
-
-
 #
 # TODO: Load up the dataset into dataframe 'X'
 #
 # .. your code here ..
-
-
-
+X = pd.read_csv('Datasets/dataset-har-PUC-Rio-ugulino.csv',sep=';',decimal=',')
 #
 # TODO: Encode the gender column, 0 as male, 1 as female
 #
 # .. your code here ..
-
-
+X.gender = X.gender.map({'Woman':1,'Man':0})
 #
 # TODO: Clean up any column with commas in it
 # so that they're properly represented as decimals instead
 #
 # .. your code here ..
-
-
 #
 # INFO: Check data types
 print X.dtypes
-
-
-
 #
 # TODO: Convert any column that needs to be converted into numeric
 # use errors='raise'. This will alert you if something ends up being
 # problematic
 #
 # .. your code here ..
-
-
+X = X.drop(122076,axis=0).reset_index(drop=True)
+X.z4 = pd.to_numeric(X.z4,errors='raise')
 #
 # INFO: If you find any problematic records, drop them before calling the
 # to_numeric methods above...
-
-
 #
 # TODO: Encode your 'y' value as a dummies version of your dataset's "class" column
 #
 # .. your code here ..
-
-
+y=pd.get_dummies(X['class'])
 #
 # TODO: Get rid of the user and class columns
 #
 # .. your code here ..
+X = X.drop(['user','class'],axis=1)
 print X.describe()
-
-
 #
 # INFO: An easy way to show which rows have nans in them
 print X[pd.isnull(X).any(axis=1)]
-
-
-
 #
 # TODO: Create an RForest classifier 'model' and set n_estimators=30,
 # the max_depth to 10, and oob_score=True, and random_state=0
 #
 # .. your code here ..
-
-
-
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators=30,max_depth=10,oob_score=True,random_state=0)
 # 
 # TODO: Split your data into test / train sets
 # Your test size can be 30% with random_state 7
 # Use variable names: X_train, X_test, y_train, y_test
 #
 # .. your code here ..
-
-
-
-
-
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=7,test_size=0.3)
 print "Fitting..."
 s = time.time()
 #
 # TODO: train your model on your training set
 #
 # .. your code here ..
+model.fit(X_train,y_train)
 print "Fitting completed in: ", time.time() - s
-
-
 #
 # INFO: Display the OOB Score of your data
 score = model.oob_score_
@@ -106,6 +84,7 @@ s = time.time()
 # TODO: score your model on your test set
 #
 # .. your code here ..
+score = model.score(X_test,y_test)
 print "Score: ", round(score*100, 3)
 print "Scoring completed in: ", time.time() - s
 
